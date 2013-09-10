41bbd5fe9117c5a6beeb739401f64a0bd448c048
eviekassela <eviekassel@gmail.com>
2013-09-10 17:18:31 +0300

    added billing info

--- Instance.py
+++ Instance.py
@@ -5,8 +5,9 @@ Created on Sep 9, 2013
 '''
 #!/usr/bin/env python
 
-import boto.ec2
-import sys, os, time
+import boto.ec2, boto.ec2.cloudwatch
+#from boto.fps.connection import FPSConnection
+import sys, os, time, datetime
 from ConfigParser import ConfigParser
 
 class InstanceHandler:
@@ -28,7 +29,7 @@ class InstanceHandler:
         self.instances = []
     
     def launch(self):
-        print "Perform configurations in AWS ec2 and launch instances.."
+        print "Perform configurations in AWS EC2 and launch instances.."
         conn = boto.ec2.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
                                           aws_secret_access_key=self.aws_secret_access_key)
         # Create private key and save to disk
@@ -139,11 +140,45 @@ class InstanceHandler:
                     break
         conn.close()
         return hosts, ips
+    
+    def get_billing(self):
+#        fps_conn = FPSConnection(aws_access_key_id=self.aws_access_key_id,
+#                                 aws_secret_access_key=self.aws_secret_access_key)
+#        fps_conn.get_account_activity(StartDate='01-09-2013')
+#        fps_conn.close()
+        conn = boto.ec2.cloudwatch.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
+                                                     aws_secret_access_key=self.aws_secret_access_key)
+        #metrics = conn.list_metrics()
+        #print metrics
+        metrics = conn.list_metrics(metric_name=u'EstimatedCharges',namespace=u'AWS/Billing')
+        end = datetime.datetime.now()
+        start = end - datetime.timedelta(hours=12)
+        print 'Service\t\t\tLatest update\t\tEstimated charges'
+        for m in metrics[0:len(metrics)-1]:
+            datapoints = m.query(start, end, 'Sum')
+            if datapoints:
+                sys.stdout.write(m.dimensions[u'ServiceName'][0])
+                sys.stdout.write('\t\t'+str(datapoints[0][u'Timestamp']))
+                sys.stdout.flush()
+                print '\t' +str(datapoints[0][u'Sum']) + ' '+ m.dimensions[u'Currency'][0]
+        metrics = conn.list_metrics(metric_name=u'NetworkIn',namespace=u'AWS/EC2')
+        end = datetime.datetime.now()
+        start = end - datetime.timedelta(hours=24)
+        print 'AmazonEC2 total data transfered in:',
+        data = 0
+        for m in metrics[0:len(metrics)]:
+            datapoints = m.query(start, end, 'Sum')
+            if datapoints:
+                data = data + datapoints[0][u'Sum']
+        print str(data) + ' Bytes'
+        conn.close()
 
 if __name__ == "__main__":    
     instances = InstanceHandler()
     if len(sys.argv) == 2 and 'launch' == sys.argv[1]:
         instances.launch()
+    elif len(sys.argv) == 2 and 'get_billing' == sys.argv[1]:
+        instances.get_billing()
     elif len(sys.argv) > 2 and 'terminate' == sys.argv[1]:
         instances.get_instances()
         instances.terminate()
