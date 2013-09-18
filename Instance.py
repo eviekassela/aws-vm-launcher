26fadabd1e595c346de93dac4616a8b8b88c55a7
eviekassela <eviekassel@gmail.com>
2013-09-18 23:00:59 +0300

    Added alarm and spot request

--- Instance.py
+++ Instance.py
@@ -6,8 +6,7 @@ Created on Sep 9, 2013
 #!/usr/bin/env python
 
 import boto.ec2, boto.ec2.cloudwatch
-#from boto.fps.connection import FPSConnection
-import sys, os, time, datetime
+import sys, os, time, datetime, thread
 from ConfigParser import ConfigParser
 
 class InstanceHandler:
@@ -27,9 +26,27 @@ class InstanceHandler:
         self.hostname_template = cfg.get("config", "hostname_template")
         self.availabity_zone = cfg.get("config", "availabity_zone")
         self.instances = []
+        self.alarm = []
+        self.initial_charges = 0
+        self.charges_limit = 0
+    
+    def authenticate(self):
+        print 'Checking if user has access rights..'
+        db = MySQLdb.connect(host="localhost", user="root", passwd="evie", db="aws_users")
+        cur = db.cursor() 
+        cur.execute("select * from aws_auth where user='evie'")
+        result = cur.fetch_all()
+        if result:
+            for row in result:
+                print row
+            print 'Authentication succeded. Access granted.'
+            self.charges_limit = 0 #<---- to be fixed
+        else:
+            print 'Authentication failed. Please contact owner company to obtain access rights.'
+            sys.exit(2)
     
     def launch(self):
-        print "Perform configurations in AWS EC2 and launch instances.."
+        print "Perform configurations in AWS EC2 and request spot instances.."
         conn = boto.ec2.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
                                           aws_secret_access_key=self.aws_secret_access_key)
         # Create private key and save to disk
@@ -39,14 +56,32 @@ class InstanceHandler:
 #        self.group = conn.create_security_group(name=self.security_group, description=self.security_group)
 #        self.group.authorize('tcp', 22, 22, '0.0.0.0/0')
         # Launch instances
-#        reservation = conn.run_instances(self.image, min_count=self.count, max_count=self.count,
-#                                         key_name=self.key, security_groups=[self.group], 
-#                                         instance_type=self.instance_type, placement=self.availabity_zone)
-        reservation = conn.run_instances(self.image, min_count=self.count, max_count=self.count,
-                                         key_name=self.key, security_groups=[self.security_group], 
-                                         instance_type=self.instance_type, placement=self.availabity_zone)
+#        start = datetime.datetime.now().isoformat()
+#        end = (datetime.datetime.now() + datetime.timedelta(minutes=5)).isoformat()
+        requests = conn.request_spot_instances('0.008', self.image, count=self.count, type='one-time',
+                                              #valid_from=start, valid_until=end, launch_group='group_devil',
+                                              key_name=self.key, security_groups=[self.security_group],
+                                              #user_data=????,
+                                              instance_type=self.instance_type)
+                                              #placement=self.availabity_zone)
+        fulfilled = 0
+        while fulfilled == 0:
+            fulfilled = 1
+            reqs = ""
+            for request in requests:
+                if str(conn.get_all_spot_instance_requests(request_ids=[str(request.id)])[0].status.code) != 'fulfilled':
+                    reqs+=str(request.id)+' '
+                    fulfilled = 0
+            if reqs != "":
+                print "waiting on requests: " + reqs
+            else:
+                print "Spot instances request fulfilled"
+            time.sleep(10)
+        for request in requests:
+            iid = conn.get_all_spot_instance_requests(request_ids=[str(request.id)])[0].instance_id
+            reservation = conn.get_all_instances(filters={'instance-id': iid})
+            self.instances = self.instances + reservation[0].instances
         # Wait for all instances to be up and running
-        self.instances = reservation.instances
         for instance in self.instances:
             status = instance.update()
             while status == 'pending':
@@ -71,10 +106,10 @@ class InstanceHandler:
         return hosts, ips
     
     def terminate(self):
-        conn = boto.ec2.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
-                                          aws_secret_access_key=self.aws_secret_access_key)
         if self.instances:
             print "Terminating instances.."
+            conn = boto.ec2.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
+                                              aws_secret_access_key=self.aws_secret_access_key)
             # Create a list of instances' ids
             term = []
             for instance in self.instances:
@@ -109,10 +144,13 @@ class InstanceHandler:
 #            except:
 #                print "There are remaining keypairs and/or security groups. Make sure to delete"
 #                print "them if not needed."
+            conn.close()
         else:
             print "No running instances to terminate"
-        conn.close()
-        
+        if self.alarm:
+            self.alarm.delete()
+            print 'Deleted alarm'
+    
     def get_instances(self):
         hosts = []
         ips = []
@@ -139,53 +177,105 @@ class InstanceHandler:
                     print 'Aborted.'
                     break
         conn.close()
+        if self.instances:
+            instances = ''
+            for instance in self.instances[0:len(self.instances)-1]:
+                instances += str(instance.id) + ','
+            instances += str(self.instances[len(self.instances)-1].id)
+            conn = boto.ec2.cloudwatch.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
+                                                         aws_secret_access_key=self.aws_secret_access_key)
+            self.alarm = conn.describe_alarms(alarm_names=['charges_'+instances])[0]
+            conn.close()
         return hosts, ips
     
     def get_billing(self):
-#        fps_conn = FPSConnection(aws_access_key_id=self.aws_access_key_id,
-#                                 aws_secret_access_key=self.aws_secret_access_key)
-#        fps_conn.get_account_activity(StartDate='01-09-2013')
-#        fps_conn.close()
         conn = boto.ec2.cloudwatch.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
                                                      aws_secret_access_key=self.aws_secret_access_key)
         #metrics = conn.list_metrics()
         #print metrics
-        metrics = conn.list_metrics(metric_name=u'EstimatedCharges',namespace=u'AWS/Billing')
+        metrics = conn.list_metrics(metric_name='EstimatedCharges',namespace='AWS/Billing')
         end = datetime.datetime.now()
-        start = end - datetime.timedelta(hours=12)
+        start = end - datetime.timedelta(hours=8)
         print 'Service\t\t\tLatest update\t\tEstimated charges'
         for m in metrics[0:len(metrics)-1]:
             datapoints = m.query(start, end, 'Sum')
             if datapoints:
-                sys.stdout.write(m.dimensions[u'ServiceName'][0])
-                sys.stdout.write('\t\t'+str(datapoints[0][u'Timestamp']))
+                last=len(datapoints)
+                sys.stdout.write(m.dimensions['ServiceName'][0])
+                sys.stdout.write('\t\t'+str(datapoints[0]['Timestamp']))
                 sys.stdout.flush()
-                print '\t' +str(datapoints[0][u'Sum']) + ' '+ m.dimensions[u'Currency'][0]
-        metrics = conn.list_metrics(metric_name=u'NetworkIn',namespace=u'AWS/EC2')
-        end = datetime.datetime.now()
-        start = end - datetime.timedelta(hours=24)
-        print 'AmazonEC2 total data transfered in:',
-        data = 0
-        for m in metrics[0:len(metrics)]:
-            datapoints = m.query(start, end, 'Sum')
-            if datapoints:
-                data = data + datapoints[0][u'Sum']
-        print str(data) + ' Bytes'
+                print '\t' +str(datapoints[0]['Sum']) + ' '+ m.dimensions['Currency'][0]
+        sys.stdout.write('Total charges:\t\t\t\t\t')
+        sys.stdout.flush()
+        datapoints = metrics[len(metrics)-1].query(start, end, 'Sum')
+        print str(datapoints[0]['Sum']) + ' '+ m.dimensions['Currency'][0]
+        self.initial_charges = float(datapoints[0]['Sum'])
         conn.close()
+    
+    def set_alarm(self):
+        conn = boto.ec2.cloudwatch.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
+                                                     aws_secret_access_key=self.aws_secret_access_key)
+        instances = ''
+        for instance in self.instances[0:len(self.instances)-1]:
+            instances += str(instance.id) + ','
+        instances += str(self.instances[len(self.instances)-1].id)
+        self.alarm = boto.ec2.cloudwatch.MetricAlarm(name='charges_'+instances, metric='EstimatedCharges',
+                                                     namespace='AWS/Billing', statistic='Sum', comparison='>',
+                                                     threshold=float(self.initial_charges+self.charges_limit),
+                                                     period=3600, evaluation_periods=6,
+                                                     description="Alarm to terminate instances if charges exceed limit",
+                                                     dimensions={"Currency":"USD"},
+                                                     alarm_actions=['arn:aws:sns:us-east-1:460252822275:devel_charges'])
+        conn.create_alarm(self.alarm)
+        conn.close()
+        print 'Created alarm to terminate instances if charge limit is exceeded'
+        #thread.start_new_thread(self.monitor_alarm, ())
+    
+    def monitor_alarm(self):
+        conn = boto.ec2.cloudwatch.connect_to_region(self.region, aws_access_key_id=self.aws_access_key_id,
+                                                     aws_secret_access_key=self.aws_secret_access_key)
+        metrics = conn.list_metrics(metric_name='EstimatedCharges',namespace='AWS/Billing')
+        try:
+            while True:
+                end = datetime.datetime.now()
+                start = end - datetime.timedelta(hours=8)
+                datapoints = metrics[len(metrics)-1].query(start, end, 'Sum')
+                if float(datapoints[0]['Sum']) > float(self.initial_charges+self.charges_limit):
+                    print 'Charges limit reached, shutting-down everything!'
+                    self.terminate_instances()
+                    self.get_billing()
+                    sys.exit(0)
+                time.sleep(300)
+        except (KeyboardInterrupt, SystemExit):
+            conn.close()
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     instances = InstanceHandler()
-    if len(sys.argv) == 2 and 'launch' == sys.argv[1]:
-        instances.launch()
-    elif len(sys.argv) == 2 and 'get_billing' == sys.argv[1]:
-        instances.get_billing()
-    elif len(sys.argv) > 2 and 'terminate' == sys.argv[1]:
-        instances.get_instances()
-        instances.terminate()
-    elif 'terminate' != sys.argv[1] and 'launch' != sys.argv[1]:
-        print "Unknown command"
-        sys.exit(2)
+    if len(sys.argv) == 2:
+        if 'launch' == sys.argv[1]:
+            #instances.authenticate()
+            instances.launch()
+        elif 'auth' == sys.argv[1]:
+            #instances.authenticate()
+            print 'wait'
+        elif 'get_billing' == sys.argv[1]:
+            instances.get_billing()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+    elif len(sys.argv) > 2:
+        if 'terminate' == sys.argv[1]:
+            instances.get_instances()
+            instances.terminate()
+        elif 'set_alarm' == sys.argv[1]:
+            #instances.authenticate()
+            instances.get_instances()
+            instances.get_billing()
+            instances.set_alarm()
+        else:
+            print "Unknown command"
+            sys.exit(2)
     else:
-        print "usage: %s launch|terminate instance1 instance2 .." % sys.argv[0]
+        print "usage: %s launch|auth|get_billing|terminate instance1 instance2 ..|set alarm instance1 instance2 .." % sys.argv[0]
         sys.exit(2)
     sys.exit(0)
